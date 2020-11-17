181c1d18688877739370efa9d34e126f79a8e943
Gelsey Torres <gelsey.torres@gmail.com>
2020-11-17 13:53:47 -0500

    Adds create action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,9 +6,17 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.create(name: params[:name_of_business],
+                                  chain: params[:chain],
+                                  address: params[:address],
+                                  phone_number: params[:phone_number],
+                                  hours_of_operation: params[:hours])
+    @restaurant.save
+    render 'new'
   end
 
   def update
