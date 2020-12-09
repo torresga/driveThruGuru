0d0f05568c102dacef54502d00a337083a5fa10f
Gelsey Torres <gelsey.torres@gmail.com>
2020-12-09 12:09:24 -0500

    creates search action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,5 +3,31 @@ class StaticPagesController < ApplicationController
   end
 
   def search
+    queries = []
+    non_empty_params = search_params.select {|key, value| value != "" }
+
+    if non_empty_params.has_key?("address")
+      non_empty_params["address"] = "%#{non_empty_params["address"]}%"
+    end
+
+    non_empty_params.each do |key, value|
+      if key == "address"
+        queries << "restaurants.#{key} LIKE ?"
+      elsif key == "name"
+        queries << "categories.#{key} = ?"
+      else
+        queries << "restaurants.#{key} = ?"
+      end
+    end
+
+    queries = queries.join(" AND ")
+
+    @restaurants = Restaurant.joins(:categories).where(queries, *non_empty_params.values)
+  end
+
+  private
+
+  def search_params
+    params.require(:restaurant).permit(:name, :chain, :address)
   end
 end
