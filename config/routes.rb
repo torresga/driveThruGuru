c45386d66b7354d0a005711bfbdcef72622ec79a
Gelsey Torres <gelsey.torres@gmail.com>
2020-12-14 15:24:59 -0500

    Create sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   get '/search', to: 'pages#search'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+
   resources :restaurants
   resources :users
 end
