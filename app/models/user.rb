830eabd990bdaee5e991bb2b41ef793831e42633
Gelsey Torres <gelsey.torres@gmail.com>
2020-12-14 14:30:09 -0500

    Add a validation for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_one_attached :avatar
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                                    uniqueness: true
 end
