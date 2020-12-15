1afe8ccdcebf95c7766c1e8fb89cf24ff24261d5
Gelsey Torres <gelsey.torres@gmail.com>
2020-12-15 14:45:28 -0500

    Updates users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,6 +22,10 @@ module SessionsHelper
     end
   end
 
+  def current_user?(user)
+    user && user == current_user
+  end
+
   def forget(user)
     user.forget
     cookies.delete(:user_id)
@@ -32,5 +36,13 @@ module SessionsHelper
     forget(current_user)
     reset_session
     @current_user = nil
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
