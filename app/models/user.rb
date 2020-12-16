fc8a6c300bc68b1f27d701b061e22f4350108e57
Gelsey Torres <gelsey.torres@gmail.com>
2020-12-16 09:59:57 -0500

    Add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   has_one_attached :avatar
 
+  before_save :downcase_email
+  validates :name, presence: true, length: { minimum: 1 }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+  validates :email, presence: true, length: { maximum: 255 },
+                                    format: { with: VALID_EMAIL_REGEX },
                                     uniqueness: true
 
   has_secure_password
