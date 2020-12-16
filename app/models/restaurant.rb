a254742c4a070ae7e933949aeac82cdc8f880d5f
Gelsey Torres <gelsey.torres@gmail.com>
2020-12-16 10:19:13 -0500

    Adds validations and filters to Restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,19 @@ class Restaurant < ApplicationRecord
   has_and_belongs_to_many :categories
   has_many :business_hours
   accepts_nested_attributes_for :business_hours
+
+  validates :name, presence: true, length: { minimum: 1}
+  validates :chain, presence: true
+  validates :categories, presence: true
+  validates :address, presence: true
+  validates :business_hours, presence: true
+  validates :phone_number, presence: true
+
+  before_save :capitalize_name
+
+  private
+
+  def capitalize_name
+    self.name = name.capitalize
+  end
 end
