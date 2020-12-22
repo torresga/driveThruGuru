9d394c57e1cb656d9da26b74cb2def3f00fa35ba
Gelsey Torres <gelsey.torres@gmail.com>
2020-12-22 16:49:10 -0500

    adds link and changes the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_21_160133) do
+ActiveRecord::Schema.define(version: 2020_12_22_192236) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(version: 2020_12_21_160133) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
